10c491e8a5048bffc9f31fe7440727f74ae74df1
Gary Miller <gary.miller@gmx.com>
2020-02-05 11:54:55 -0500

    Update noko_practice_nyt.rb
    Adding in the closing )

--- nokogiri_practice_nyt.rb
+++ nokogiri_practice_nyt.rb
@@ -5,7 +5,7 @@ require 'sinatra'
 browser = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4)
 AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1'
 
-doc = Nokogiri::HTML(open ('http://nytimes.com', browser)
+doc = Nokogiri::HTML(open ('http://nytimes.com', browser))
 
 nyt_headline = doc.at_css('h2 span').content
 
