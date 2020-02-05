74607e3b31cd790054c927a3d4404649ae034f9b
Gary Miller <gary.miller@gmx.com>
2020-02-05 11:20:22 -0500

    Update nokogiri_practice_nyt.rb
    Adding in Sinatra gem for HTML output.

--- nokogiri_practice_nyt.rb
+++ nokogiri_practice_nyt.rb
@@ -1,8 +1,16 @@
 require 'open-uri'
 require 'nokogiri'
+require 'sinatra'
 
 doc = Nokogiri::HTML(open 'http://nytimes.com')
 
 nyt_headline = doc.at_css('h2 span').content
 
 nyt_url = "http://nytimes.com" + doc.at_css('.css-16ugw5f a')[:href]
+
+html = "<h1>Nokogiri News Service</h1>"
+html += "<h2>Top Story: <a href=\"#{nyt_url}\">#{nyt_headline}</a></h2>"
+
+get '/' do
+      html
+end
